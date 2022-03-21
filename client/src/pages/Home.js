8152783f18fe29f957533e89bfa1bcfb00af4ca8
ebeltz <ebbeltz@gmail.com>
2022-03-21 18:32:41 -0400

    moved homepage text above carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,15 @@ import Carousel3 from '../assets/marleyandme.jpg';
 const Home = () => {
   return (
     <div>
+      <h1>Welcome to talkie-box.</h1>
+      <section>
+        <h2>What are you watching?</h2>
+        <p>Share which movies have caught your attention. Let your friends know whether those films are HOT or NOT.</p>
+      </section>
+      <section>
+        <h2>What are your friends watching?</h2>
+        <p>Chances are others are interested in the same movies as you. Find out what they are labeling as HOT or NOT.</p>
+      </section>
       <Carousel class="carousel">
         <Carousel.Item>
           <img
@@ -45,15 +54,6 @@ const Home = () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <h1>Welcome to talkie-box.</h1>
-      <section>
-        <h2>What are you watching?</h2>
-        <p>Share which movies have caught your attention. Let your friends know whether those films are HOT or NOT.</p>
-      </section>
-      <section>
-        <h2>What are your friends watching?</h2>
-        <p>Chances are others are interested in the same movies as you. Find out what they are labeling as HOT or NOT.</p>
-      </section>
     </div>
   );
 };
