e8f1ba988a67cd86abf54617526e2346d40d8ba0
The Prattler <jmichaelsmith@juno.com>
2022-03-22 15:20:19 -0400

    hardcoded discussion page with functioning hot/cold buttons

--- client/src/pages/Discussion.js
+++ client/src/pages/Discussion.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Discussion = () => {
+    // const [hotVotes, setHotVotes] = useState(0);
+    // const [coldVotes, setColdVotes] = useState(0);
+    const [disableHot, setDisableHot] = useState(false);
+    const [disableCold, setDisableCold] = useState(false);
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <section>
+            <div>
+                <h1>Title: The Lord of the Rings</h1>
+                <h2>Year: 1978</h2>
+            </div>
+            <div>
+                <img></img>
+                <p>
+                Plot: The Fellowship of the Ring embark on a journey to destroy the One Ring and end Sauron's reign over Middle-earth. 
+                </p>
+            </div>
+            <div>
+                <button disabled={disableHot} onClick={() => (setDisableHot(true), setDisableCold(false))}>HOT</button>
+                <button disabled={disableCold} onClick={() => (setDisableCold(true), setDisableHot(false))}>NOT</button>
+            </div>
+            <div>
+                <h3>
+                    User Reviews
+                </h3>
+                <p>
+                    "Lorem ipsum bo diddley protego wingarium haberdashery cauliflower."
+                </p>
+                <p>
+                    Reviewed by username on 01/01/2022
+                </p>
+            </div>
+        </section>
     )
 }
 
